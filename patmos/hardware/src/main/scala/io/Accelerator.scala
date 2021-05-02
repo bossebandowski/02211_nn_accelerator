acb4c524ca9fbcdb611c7e135dcf9c0abd41a453
Bosse Bandowski <bosse.bandowski@outlook.com>
2021-05-02 12:24:01 +0200

    added most of first layer logic, state transitions seem to work, neuron values not checked

--- patmos/hardware/src/main/scala/io/Accelerator.scala
+++ patmos/hardware/src/main/scala/io/Accelerator.scala
@@ -25,8 +25,10 @@ class Accelerator() extends CoreDevice() {
   val wReg = Reg(init = SInt(0, 32))
   val bReg = Reg(init = SInt(0, 32))
 
-  val layer1 = Vec(100, Reg(init = SInt(0, 32)))
-  val layer2 = Vec(10, Reg(init = SInt(0, 32)))
+  val layer1 = Reg(Vec(100, SInt(32.W)))
+  val layer2 = Reg(Vec(10, SInt(32.W)))
+
+  val nodeIdx = Reg(init = UInt(0, 8))
 
   // memory
   val memory = Module(new MemorySInt(18, 32))
@@ -48,8 +50,9 @@ class Accelerator() extends CoreDevice() {
   // network constants
 
   // states
-  val nonn :: loadnn :: idle :: infload :: infrun :: loadpx :: loadw :: loadb :: clear :: Nil = Enum(UInt(), 9)
-  val stateReg = Reg(init = nonn)
+  val noNN :: loadNN :: idle :: infLoad :: loadpx :: loadw0 :: incNode0  :: loadb0 :: addb0 :: infInit1 :: loadw1 :: incNode1 :: initb1 :: loadb1 :: addb1 :: writeRes :: clear :: Nil = Enum(UInt(), 17)
+  //  0       1         2       3          4         5         6            7         8         9        10          11        12          13        14        15       16          17                                 
+  val stateReg = Reg(init = noNN)
 
   // set memory address. Only for debugging
   when (masterReg.Cmd === OcpCmd.WR && masterReg.Addr(4,0) === 0x10.U) {
@@ -58,14 +61,24 @@ class Accelerator() extends CoreDevice() {
   }
   .otherwise {
     // state machine goes here
-    when (stateReg === nonn && masterReg.Cmd === OcpCmd.WR) {
-        io.ocp.S.Resp := OcpResp.DVA
-        when (masterReg.Data === UInt(0)) {
-          stateReg := loadnn
-          writeAddr := weightAddrZero
+
+    when (stateReg === noNN && masterReg.Cmd === OcpCmd.WR) {
+      /*
+      initial state. No network is loaded, so inference is not possible.
+      Waiting for a write command and data == 0 to transition to load network state
+      */
+      io.ocp.S.Resp := OcpResp.DVA
+      when (masterReg.Data === UInt(0)) {
+        stateReg := loadNN
+        writeAddr := weightAddrZero
       }
     }
-    when (stateReg === loadnn && masterReg.Cmd === OcpCmd.WR) {
+
+    when (stateReg === loadNN && masterReg.Cmd === OcpCmd.WR) {
+      /*
+      load neural network state. whenever we get a write command, we store the current ocp data val
+      in memory and increment the memory address
+      */
       io.ocp.S.Resp := OcpResp.DVA
       memory.io.wrEna := true.B
       memory.io.wrAddr := writeAddr
@@ -75,19 +88,31 @@ class Accelerator() extends CoreDevice() {
       }
       writeAddr := writeAddr + UInt(1)
     }
+
     when (stateReg === idle && masterReg.Cmd === OcpCmd.WR) {
+      /*
+      idle state. The accelerator has a network loaded in its memory and it is waiting for
+      commands from the patmos. You can either load a new network by sending "0" or start
+      the inference process by sending "1"
+      */
       io.ocp.S.Resp := OcpResp.DVA
       memory.io.wrEna := false.B
       when(masterReg.Data(0) === UInt(0)) {
-        stateReg := loadnn
+        stateReg := loadNN
         writeAddr := weightAddrZero
       }
       .otherwise {
-        stateReg := infload
+        stateReg := infLoad
         writeAddr := imgAddrZero
       }
     }
-    when (stateReg === infload && masterReg.Cmd === OcpCmd.WR) {
+
+    when (stateReg === infLoad && masterReg.Cmd === OcpCmd.WR) {
+      /*
+      first inference state. In order to run inference, you must first load an image into
+      the accelerator memory. Provide sequential pixel values on the ocp data line and they
+      will be stored in memory. The address increments automatically.
+      */
       io.ocp.S.Resp := OcpResp.DVA
       memory.io.wrEna := true.B
       memory.io.wrAddr := writeAddr
@@ -96,12 +121,58 @@ class Accelerator() extends CoreDevice() {
         readAddrP := imgAddrZero
         readAddrW := weightAddrZero
         readAddrB := biasAddrZero
-        stateReg := infrun
+        memory.io.rdAddr := imgAddrZero
+        stateReg := loadpx
       }
       writeAddr := writeAddr + UInt(1)
     }
-    when (stateReg === infrun) {
-        memory.io.wrEna := false.B
+
+    when (stateReg === loadpx) {
+      /*
+      the image in the memory is analysed pixel by pixel. For every pixel, we load the weights
+      to all neurons in the hidden layer, multiply them with the pixel value, and increment the
+      value of the respective neuron. Once all pixels have been analysed, we transition to the
+      second layer (output layer). No responses to write commands are issued while the accelerator
+      is busy with inference.
+      */
+      when (readAddrP === weightAddrZero) {
+        memory.io.rdAddr := readAddrB         // set up bias read
+        nodeIdx := UInt(0)                    // reset node idx
+        readAddrB := readAddrB + UInt(1)      // increment bias addr
+        readAddrW := readAddrW + UInt(1)      // also need to increment weight address
+        stateReg := loadb0
+      }
+      .otherwise {
+        memory.io.wrEna := false.B            // disable memory write to prevent data corruption
+        pReg := memory.io.rdData              // read pixel value into corresponding register
+        readAddrP := readAddrP + UInt(1)      // increment pixel address to be ready for next read
+        nodeIdx := UInt(0)                    // for every pixel, we start at node 0 in the hidden layer
+        memory.io.rdAddr := readAddrW         // we will read a weight in the next state. Set address in preparation
+        readAddrW := readAddrW + UInt(1)      // increment weight addr
+        stateReg := incNode0
+      }
+    }
+
+    when (stateReg === incNode0) {
+      /*
+      incrementing node values. Transitions back to pixel load when all weights for a single pixel have
+      been evaluated. Otherwise moves on to the next weight.
+      */
+      when (nodeIdx < UInt(100)) {
+                                            // increment node value with product of pixel and weight (directly from memory to save clock cycles)
+        layer1(nodeIdx) := layer1(nodeIdx) + pReg * memory.io.rdData
+        memory.io.rdAddr := readAddrW       // update memory address for next cycle
+        readAddrW := readAddrW + UInt(1)    // increment read address for next cycle
+        nodeIdx := nodeIdx + UInt(1)        // increment node index for next cycle
+      }
+      .otherwise {
+        memory.io.rdAddr := readAddrP       // prepare for pixel load. No need to increment weight address here because that happens in the pixel load state
+        stateReg := loadpx
+      }
+    }
+
+    when (stateReg === loadb0) {
+
     }
   }
 
